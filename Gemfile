9d36b6dc1123d04511374c342692fa9617b76834
Asjad <iftikharasjad@gmail.com>
2022-09-22 17:16:50 +0500

    News Feed page added

--- Gemfile
+++ Gemfile
@@ -74,5 +74,6 @@ end
 gem 'devise'
 gem 'cancancan'
 gem 'activeadmin'
+gem 'nokogiri'
 gem "font-awesome-sass", "~> 6.2.0"
 gem "tailwindcss-rails", "~> 2.0"
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :tournaments
+  get '/news', to: 'application#show_news'
 
   # Defines the root path route ("/")
   root "home#index"
