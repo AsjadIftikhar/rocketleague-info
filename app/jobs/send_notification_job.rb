522651702e687f9ad0e31b55420cdedccbe91680
Asjad <iftikharasjad@gmail.com>
2022-09-28 10:39:40 +0500

    Email Complete

--- app/jobs/send_notification_job.rb
+++ app/jobs/send_notification_job.rb
@@ -4,12 +4,16 @@ class SendNotificationJob < ApplicationJob
   def perform(*args)
     current_time = Time.zone.now.change({ min: 0, sec: 0 })
 
-    testing = Time.zone.now.change({hour: 16, min: 0, sec: 0 })
+    # testing = Time.zone.now.change({hour: 16, min: 0, sec: 0 })
 
     UserFollowMatch.joins(:match).where('matches.date >= ?', current_time).each do |f|
-      if testing + f.hours_before_notification.hour == f.match.date.change({ min: 0, sec: 0 })
+      if current_time + f.hours_before_notification.hour == f.match.date.change({ min: 0, sec: 0 })
         NotificationMailer.with(match: f.match).notification_email(args[0]).deliver_now
       end
+
+      # if testing + f.hours_before_notification.hour == f.match.date.change({ min: 0, sec: 0 })
+      #   NotificationMailer.with(match: f.match).notification_email(args[0]).deliver_now
+      # end
     end
   end
 end
