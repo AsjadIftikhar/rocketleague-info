f7c17a1bc16d422885b01c6223d24206a78af3b3
Asjad <iftikharasjad@gmail.com>
2022-09-29 10:16:08 +0500

    Simple Search Added

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,6 +2,15 @@ class PlayersController < InheritedResources::Base
   before_action :authenticate_user!
   load_and_authorize_resource
 
+  def index
+    query = params[:q]
+    if not query.empty?
+      @players = Player.where("name = ? or country = ?", query, query)
+    else
+      @players = Player.all
+    end
+  end
+
   private
 
     def player_params
